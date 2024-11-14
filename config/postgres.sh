0723a509dcb0bd26e2d24855366f966f896fb3b1
Hermann Mayer <hermann.mayer92@gmail.com>
2024-11-14 12:36:45 +0100

    Enabled logical replication by default.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- config/postgres.sh
+++ config/postgres.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-export POSTGRES_ARGS=${POSTGRES_ARGS:-''}
+# Enable logical replication by default. This allows tools like Debezium
+# directly to add an replication slot to stream events.
+export POSTGRES_ARGS=${POSTGRES_ARGS:-'-c wal_level=logical'}
 
 exec /usr/local/bin/docker-entrypoint.sh postgres ${POSTGRES_ARGS}
