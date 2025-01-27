59e4c88453192ed6a7a6422e82b1d2943e2ca1d9
Hermann Mayer <hermann.mayer92@gmail.com>
2025-01-27 11:18:05 +0100

    Dropped support for version 16.x and 17.0.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- 16.4/config/postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Enable logical replication by default. This allows tools like Debezium
-# directly to add an replication slot to stream events.
-export POSTGRES_ARGS=${POSTGRES_ARGS:-'-c wal_level=logical'}
-
-exec /usr/local/bin/docker-entrypoint.sh postgres ${POSTGRES_ARGS}
